08bda667458d6e78b79451c5ffceb367aa7b358d
beldjilali yanis <beldjilaliyanis@gmail.com>
2023-01-24 22:29:13 +0100

    24-01

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('',views.test, name="routes"),
+    path('adverts/',views.test, name="adverts"),
+    path('advert/<str:pk>',views.test, name="advert"),
+    path('saved/',views.test, name="saved"),
+    path('posted/',views.test, name="posted"),
+    path('profile/<str:pk>',views.test, name="profile")
 ]
+
+
+
